2b47bdfff5a2bacc65103267267ec04338cc02f5
Cris Fandino <sicrnujnuj@gmail.com>
2021-01-12 16:07:35 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-sdd // import resolve from 'rollup-plugin-node-resolve';
+// import resolve from 'rollup-plugin-node-resolve';
 // import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
